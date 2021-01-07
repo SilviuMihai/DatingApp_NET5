ce3e7696dce1763e36d9e6d1b7ccfca6875c7de0
SilviuMihai <silviu96@gmail.com>
2021-01-07 15:03:00 +0200

    Updating resources (End section 10)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,7 +38,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +48,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     NgbModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     {
@@ -52,6 +57,9 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     },
     {
       provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true 
+    },
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi:true 
     }
   ],
   bootstrap: [AppComponent]
